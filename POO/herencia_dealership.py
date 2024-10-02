ee80ea27550ea8810267d331597d30d5a0f473da
odelkis <odelkiseaj539@gmail.com>
2024-10-02 23:45:47 +0000

    Subir archivos

---
+++ POO/herencia_dealership.py
@@ -0,0 +1,137 @@
+class Vehicle:
+    def __init__(self, brand, model, price):
+        # Encapsulacion
+        self.brand = brand
+        self.model = model
+        self.price = price
+        self.is_available = True
+
+    def sell(self):
+        if self.is_available:
+            self.is_available = False
+            print(f"El vehiculo {self.brand}. Ha sido vendido")
+        else:
+            print(f"El vehiculo {self.brand}. No esta disponible")
+
+  # Abstraccion  
+    def check_available(self):
+        return self.is_available
+    
+      # Abstraccion  
+    def get_price(self):
+        return self.price
+    
+    def start_engine(self):
+        raise NotImplementedError("Este metodo debe ser implementado por la subclase")
+    
+    def stop_engine(self):
+        raise NotImplementedError("Este metodo debe ser implementado por la subclase")
+    
+# Herencia
+class Car(Vehicle):
+    # Polimorfismo
+    def start_engine(self):
+        if not self.is_available:
+            return f"El motor del coche {self.brand} esta en marcha"
+        else:
+            return f"El coche {self.brand} no esta disponible"
+
+     # Polimorfismo   
+    def stop_engine(self):
+        if self.is_available:
+            return f"El motor del coche {self.brand} se ha detenido"
+        else:
+            return f"El coche {self.brand} No esta disponible"
+
+# Herencia    
+class Bike(Vehicle):
+    # Polimorfismo
+    def start_engine(self):
+        if not self.is_available:
+            return f"La bicicleta {self.brand} esta en marcha"
+        else:
+            return f"La bicicleta {self.brand} no esta disponible"
+        
+        # Polimorfismo
+    def stop_engine(self):
+        if self.is_available:
+            return f"La bicicleta {self.brand} se ha detenido"
+        else:
+            return f"La bicicleta {self.brand} No esta disponible"
+        
+# Herencia
+class Truck(Vehicle):
+    # Polimorfismo
+    def start_engine(self):
+        if not self.is_available:
+            return f"El motor del camion {self.brand} esta en marcha"
+        else:
+            return f"El camion {self.brand} no esta disponible"
+        
+    # Polimorfismo
+    def stop_engine(self):
+        if self.is_available:
+            return f"El motor del camion {self.brand} se ha detenido"
+        else:
+            return f"El camion {self.brand} No esta disponible"
+        
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self.purchased_vehicles = []
+
+    def buy_vehicle(self, vehicle: Vehicle):
+        if vehicle.check_available():
+            vehicle.sell()
+            self.purchased_vehicles.append(vehicle)
+        else:
+            print(f"Lo siento, {vehicle} no esta disponible")
+    
+    def inquire_vehicle(self, vehicle: Vehicle):
+        if vehicle.check_available():
+            availability = "Disponible"
+        else:
+            availibility = "No disponible"
+        print(f"El {vehicle.brand} esta {availability} y cuesta {vehicle.get_price()}")
+
+class Dealership:
+    def __init__(self):
+        self.inventory = []
+        self.customers = []
+
+    def add_vehicles(self, vehicle: Vehicle):
+        self.inventory.append(vehicle)
+        print(f"El {vehicle.brand} ha sido añadido al inventario")
+
+    def register_customers(self, customer: Customer):
+        self.customers.append(customer)
+        print(f"El {customer.name} ha sido añadido")
+
+    def show_available_vehicle(self):
+        print("Vehiculos disponibles en la tienda")
+        for vehicle in self.inventory:
+            if vehicle.check_available():
+                print(f"- {vehicle.brand} por {vehicle.get_price()}")
+
+car1 = Car("Toyota", "Corolla", 20000)
+bike1 = Bike("Yamaha", "MT-07", 7000)
+truck1 = Truck("Volvo", "FH16", 80000)
+
+customer1 = Customer("Carlos")
+
+dealership = Dealership()
+dealership.add_vehicles(car1)
+dealership.add_vehicles(bike1)
+dealership.add_vehicles(truck1)
+
+# Mostrar vehiculos disponibles
+dealership.show_available_vehicle()
+
+# Cliente consultar un vehiculo
+customer1.inquire_vehicle(car1)
+
+# Cliente consultar un vehiculo
+customer1.buy_vehicle(car1)
+
+# Mostrar vehiculos disponibles
+dealership.show_available_vehicle()
