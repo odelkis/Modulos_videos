4b2e6479ae7e780b6e76f8c3867448f3938f8a74
odelkis <odelkiseaj539@gmail.com>
2024-10-06 15:45:23 +0000

    Subir archivos

---
+++ Lectura_Escritura/new_data_csv.py
@@ -0,0 +1,14 @@
+import csv
+new_product = {
+    'name': "Wireless Chatger",
+    'price': 75,
+    'quantity': 100,
+    'brand': 'ChargerMaster',
+    'category': 'Accesories',
+    "entry_date": '2024-07-01'
+}
+
+with open('products.csv', mode='a', newline='') as file:
+    file.write('\n')
+    csv_writer = csv. DictWriter(file, fieldnames = new_product.keys())
+    csv_writer.writerow(new_product)
